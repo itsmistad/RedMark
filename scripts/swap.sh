03add03b62adef56960438a611d6724c2b92922f
Derek <d@mistad.net>
2019-08-18 00:06:16 -0500

    Fixed cart counter not updating on the first product
    - Added swatch.liquid back
    - Majorly cleaned up product-template.liquid
    - Added environment verification to swap.sh
    - Added language strings for "Head to checkout" button

--- scripts/swap.sh
+++ scripts/swap.sh
@@ -2,6 +2,23 @@
 
 FORCE=$1
 
+# Credit goes to Stefan Farestam from StackOverflow
+function parse_yaml {
+    local prefix=$2
+    local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+    sed -ne "s|^\($s\):|\1|" \
+        -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+    awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
 function force {
     DIR=${PWD##*/}
     if [[ $DIR == "scripts" ]]; then
@@ -21,6 +38,14 @@ if [[ $FORCE == "-f" ]] || [[ $FORCE == "--force" ]]; then
 fi
 
 if [[ $CONT = false ]]; then
+    exit
+fi
+
+eval $(parse_yaml config.yml)
+ENV_STORE=$ENV"_store"
+if [[ -z ${!ENV_STORE+x} ]]; then
+    echo "The environment '$ENV' is not defined in config.yml!"
+    CONT=false
     exit
 fi
 
